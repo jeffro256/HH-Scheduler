00c15a30fc8d6dae832d77d5e70e3364dfbe4e9c
Jeffrey A. Ryan <jeffaryan7@gmail.com>
2016-09-25 18:19:59 -0500

    Bones with Schedule Scroller

--- HH Scheduler/ScheduleViewController.swift
+++ HH Scheduler/ScheduleViewController.swift
@@ -50,17 +50,13 @@ class Schedule: NSObject, NSCoding {
     }
 }
 
-class ScheduleViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class ScheduleViewController: UIViewController {
     static var schedule: Schedule?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        /* RIP Gradient
-        let backView = UIView(frame: self.view.frame)
-        addGradient(to: backView)
-        self.collectionView?.insertSubview(backView, atIndex: 0)
-        */
+        addGradient(to: view)
 
         if ScheduleViewController.schedule == nil {
             ScheduleViewController.loadSchedule()
@@ -69,56 +65,9 @@ class ScheduleViewController: UICollectionViewController, UICollectionViewDelega
         try! ScheduleViewController.schedule?.saveToFile(schedule_file_url)
     }
 
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6*18;
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let identifier = "schedule_reuse"
-
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
-
-        if let cell_label = cell.viewWithTag(101) as? UILabel {
-            cell_label.text = String((indexPath as NSIndexPath).row)
-        }
-
-        cell.layer.borderColor = UIColor.black.cgColor
-        cell.layer.borderWidth = 0.5
-
-        return cell
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let cellHeight = collectionView.frame.height / 6
-        let cellWidth = cellHeight * 1.5
-
-        return CGSize(width: cellWidth, height: cellHeight)
-    }
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return 1;
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "Header", for: indexPath)
-
-        return headerView
-    }
-
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        let headerSize = collectionView.frame.width / 5.0
-        return CGSize(width: headerSize, height: 0)
-    }
-
-    @IBAction func handleSwipes(_ sender: AnyObject) {
-        let tabIndex = tabBarController!.selectedIndex
-
-        if (sender.direction == .right && tabIndex > 0) {
-            tabBarController!.selectedIndex -= 1
-        }
-
-        if (sender.direction == .left && tabIndex < tabBarController!.viewControllers!.count - 1) {
-            tabBarController!.selectedIndex += 1
+    override func viewDidAppear(_ animated: Bool) {
+        if let scrollView = view.viewWithTag(10) as? UIScrollView {
+            scrollView.contentSize = CGSize(width: scrollView.frame.height * 1.5, height: scrollView.frame.height)
         }
     }
 
@@ -131,10 +80,4 @@ class ScheduleViewController: UICollectionViewController, UICollectionViewDelega
             ScheduleViewController.schedule = Schedule()
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
-
