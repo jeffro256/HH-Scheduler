d00fcd70e22eedcc7b9aabaf52e8d735e6f4094a
Jeffrey A. Ryan <jeffaryan7@gmail.com>
2016-09-27 17:42:42 -0500

    Added common file and renamed elements

---
+++ HH Scheduler/Common.swift
@@ -0,0 +1,30 @@
+//
+//  Paths.swift
+//  HH Scheduler
+//
+//  Created by Jeffrey Ryan on 9/5/16.
+//  Copyright © 2016 Jeffrey Ryan. All rights reserved.
+//
+
+import Foundation
+
+enum ViewControllerIndexes: Int {
+    case Dashboard = 0
+    case Schedule
+}
+
+let document_dirs = NSSearchPathForDirectoriesInDomains(
+    .documentDirectory,
+    .userDomainMask, true)
+let document_dir = document_dirs.first!
+let document_dir_url = URL(fileURLWithPath: document_dir)
+
+let schedule_file_name = "schedule.txt"
+let schedule_file_url: URL =
+    document_dir_url.appendingPathComponent(schedule_file_name)
+
+let schedule_info_web_url =
+    URL(string: "http://jeffaryan.com/schedule_keeper/hh_schedule_info.txt")!
+let schedule_info_cache_file_name = "sinfo.cache.txt"
+let schedule_info_cache_file_url: URL =
+    document_dir_url.appendingPathComponent(schedule_info_cache_file_name)
