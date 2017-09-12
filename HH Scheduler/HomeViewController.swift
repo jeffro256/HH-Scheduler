a8464583fe52c4ea78d1d7196fe5ed4eda7fe4a4
Jeffrey A. Ryan <jeffaryan7@gmail.com>
2017-09-11 19:03:43 -0500

    Added HomeViewController

---
+++ HH Scheduler/HomeViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.swift
+//  HH Scheduler
+//
+//  Created by Jeffrey Ryan on 9/10/17.
+//  Copyright © 2017 Jeffrey Ryan. All rights reserved.
+//
+
+import UIKit
+import UICircularProgressRing
+
+class HomeViewController: UIViewController {
+    @IBOutlet weak var progessRing: UICircularProgressRingView!
+
+    @IBOutlet var labels: [UILabel]!
+}
